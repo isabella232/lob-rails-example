03979d6fb6392ff82f52d45f332547dbf13e305f
Anthony Chen <pandaman0529@gmail.com>
2017-01-06 16:09:27 -0800

    Upgrading lob wrapper version to 3.0.0 + refactors

--- app/controllers/postcards_controller.rb
+++ app/controllers/postcards_controller.rb
@@ -1,14 +1,17 @@
 class PostcardsController < ApplicationController
+  # Replace with your API key
+  LOB_API_KEY = "test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc"
 
   def index
   end
 
   def create
-    puts params[:postcards]
+    lob_client = Lob::Client.new(api_key: LOB_API_KEY)
 
-    lob = Lob.load(api_key: "test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc")
+    template_file = ERB.new(File.open(File.join(Rails.root, 'app', 'views', 'postcards', 'postcard_front.html.erb')).read)
+    custom_html = template_file.result(binding)
 
-    @results = lob.postcards.create(
+    @results = lob_client.postcards.create(
       description: "Demo Postcard",
       to: {
         name: params[:postcards][:to_name],
@@ -26,8 +29,8 @@ class PostcardsController < ApplicationController
         address_zip: params[:postcards][:from_zip],
         address_country: "US",
       },
-      front: "<html>test</html>",
-      message: params[:postcards][:message],
+      front: custom_html,
+      message: params[:postcards][:message]
     )
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   root to: redirect('/postcards')
   resources :postcards, only: [:index, :create]
 
